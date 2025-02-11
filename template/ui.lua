f09077430c64e56f1aff9fbcf746bc79b867f9b9
Reomodz <98868554+Reomodz@users.noreply.github.com>
2025-02-11 21:04:01 +0530

    Upload template/ui.lua

---
+++ template/ui.lua
@@ -0,0 +1,22 @@
+---@class ui
+---@field SimpleExpandableList fun(layout:layout, onSelectedListener?:function):android.widget.ExpandableListView @Deprecated, recommended SimpleExpandableListView
+---@field SimpleExpandableListView fun(layout:layout):android.widget.ExpandableListView
+---@field SimpleSpinner fun(layout:layout):android.widget.Spinner
+---@field SimpleListView fun(layout:ListView_layout):android.widget.ListView
+---@field SimplePopupMenu fun(layout:PopupMenu_layout):android.widget.PopupMenu
+---@
+---@field ListView fun(layout:ListView_layout):android.widget.ListView
+---@field FileListView fun(layout:ListView_layout):android.widget.ListView
+---@field GifImageView fun(layout:ImageView_layout):cn.rlyun.ui.widget.GifImageView
+---@
+---@field DrawerLayoutx fun(layout:ViewGroup_layout):androidx.drawerlayout.widget.DrawerLayout
+---@field ViewPager fun(layout:ViewGroup_layout):androidx.viewpager.widget.ViewPager
+---@field VerticalViewPager fun(layout:ViewGroup_layout):androidx.viewpager.widget.ViewPager
+---@
+---@field BaseAdapter fun(proxy:proxyMethod):android.widget.BaseAdapter
+---@field SimpleViewPagerAdapter fun(views:java.util.ArrayList):androidx.viewpager.widget.PagerAdapter
+---@
+ui = {}
+
+
+
